075253d5ad3f9234aede55454d3c25f5586b1ed9
Nissi <109863086+nissinissinissi@users.noreply.github.com>
2022-08-03 06:22:42 -0500

    Update main.swift

--- 4 urok dx/main.swift
+++ 4 urok dx/main.swift
@@ -8,4 +8,35 @@
 import Foundation
 
 print("Hello, World!")
+ var words1: [String] = ["Иногда","хватает", "мгновения", "чтобы", "забыть", "жизнь", "а", "иногда", "не", "хватает", "жизни", "чтобы", "забыть", "мгновение"]
+    
+print (words1)
 
+print (" по убыванию ")
+
+let descendingStrings = words1.sorted{(a,b) -> Bool in
+    if a.count == b.count {
+        return a < b
+    }else {
+        return a.count > b.count
+    }
+}
+print (descendingStrings)
+
+ ("по возрастанию")
+var words2: [String] = ["Иногда","хватает", "мгновения", "чтобы", "забыть", "жизнь", "а", "иногда", "не", "хватает", "жизни", "чтобы", "забыть", "мгновение"]
+let descendingString = words2.sorted {(a, b) ->  Bool in
+    return a.count < b.count
+}
+print (descendingString)
+
+var longWords = ["Иногда","хватает", "мгновения", "чтобы", "забыть", "жизнь", "а", "иногда", "не", "хватает", "жизни", "чтобы", "забыть", "мгновение"]
+var longest = 0
+let array = ["Иногда","хватает", "мгновения", "чтобы", "забыть", "жизнь", "а", "иногда", "не", "хватает", "жизни", "чтобы", "забыть", "мгновение"]
+
+if let max = array.max(by: {$1.count > $0.count}) {
+    print("the longest word is -\(max)")
+}
+if let min = array.min(by: {$0.count < $1.count}) {
+    print ("the shortest word is - \(min)")
+}
